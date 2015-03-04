dec5eff0e9313b331c22ff7f2cb11585f63147e1
daiqingbo <Dqb169@163.com>
2015-03-04 09:44:49 +0800

    A test for cocoaPods
    A test for cocoaPods

---
+++ CocoaPodsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoaPodsTest
+//
+//  Created by DQB on 15/3/2.
+//  Copyright (c) 2015年 DQB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocoaPodsTest/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  CocoaPodsTest
+//
+//  Created by DQB on 15/3/2.
+//  Copyright (c) 2015年 DQB. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AFNetworking.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
